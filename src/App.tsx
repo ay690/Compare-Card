710cc663d35a050e29d4b577d48aaf296197e65f
Aniket Yadav <aniketyadav690@gmail.com>
2025-06-11 21:28:09 +0530

    UI: Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
-
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Index />} />
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
