469d9f1479b4afeda267466ea7aff8d5a4700ea1
Aniket Yadav <aniketyadav690@gmail.com>
2025-06-11 21:31:52 +0530

    feat: Compare page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Compare from "./pages/Compare";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/compare" element={<Compare />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Compare.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Compare = () => {
+  return (
+    <div>
+      Compare Page
+    </div>
+  )
+}
+export default Compare
